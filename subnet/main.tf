67e60874f7b53b029c6f0e47e7230c2d00785993
Ibrokhimx <142237583+Ibrokhimx@users.noreply.github.com>
2023-11-08 18:26:10 -0500

    added vpc and subnet configuration

---
+++ subnet/main.tf
@@ -0,0 +1,26 @@
+variable "dynamic" {
+  type = string
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
+  default = {}
+}
+
+resource "aws_subnet" "main" {
+  for_each          = var.subnets
+  vpc_id            = var.dynamic
+  cidr_block        = each.value.cidr_block #cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+  availability_zone = each.value.availability_zone
+  tags = {
+    Name = join("-", [var.prefix, "subnet"])
+  }
+}
+
+
+variable "prefix" {
+  type = string
+}
